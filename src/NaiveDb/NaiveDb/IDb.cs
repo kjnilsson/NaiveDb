cd5c62953a70d8d6f5817067c35db285f428327b
Karl Nilsson <kjnilsson@gmail.com>
2012-12-05 19:36:58 +0000

    added INaiveDb interface

---
+++ src/NaiveDb/NaiveDb/IDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaiveDb
+{
+    public interface INaiveDb<T> where T: class
+    {
+        void Put(string key, T value);
+        T Get(string key);
+        void Delete(string key);
+    }
+}
