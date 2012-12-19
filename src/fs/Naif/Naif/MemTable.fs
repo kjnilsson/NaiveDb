9aa0937ba69b9fdcb4b6ae8324f6549b4be62b25
karl-nilsson-15below <karl.nilsson@15below.com>
2012-12-19 17:16:40 +0000

    refactoring

--- src/fs/Naif/Naif/MemTable.fs
+++ src/fs/Naif/Naif/MemTable.fs
@@ -3,49 +3,7 @@ open System.IO
 open System
 open System.Text
 
-type MemTable =
-    { Cache : Map<string, string> }
-    
-let mutable private mt = { Cache = Map.empty }
-
-let mutable private fs : FileStream = null
-
-let openFile s =
-    if(fs = null) then
-        fs <- File.OpenWrite s
-    else
-        fs.Close()
-        fs <- File.Open(s, FileMode.Create)
-
-
 let private getBytes s = Encoding.UTF8.GetBytes(s : string) : byte[]
-
-let private logWrite pair =
-    let write bytes = fs.Write(bytes, 0, bytes.Length)
-    
-    
-    match pair with
-    | (key:string, value:string) ->
-        try
-            let all = seq {
-                let keyBytes = getBytes key
-                let valueBytes = getBytes value
-                yield BitConverter.GetBytes keyBytes.Length 
-                yield keyBytes
-                yield BitConverter.GetBytes valueBytes.Length
-                yield valueBytes
-            } 
-
-            let allBytes = Array.concat all
-
-            allBytes |> write     
-
-            fs.Flush()
-            
-            Some(allBytes.Length)
-        with
-            | ex -> None
-
 
 let private toBytes(key, value) =
     let all = seq {
@@ -59,31 +17,115 @@ let private toBytes(key, value) =
 
     Array.concat all
 
+let private logWrite (fs:FileStream, pair) =
+    let write bytes = 
+        fs.Write(bytes, 0, bytes.Length)
+        fs.Flush()
+        bytes
 
-let addToMemTable (key, value) =
-    mt <- { mt with Cache =  mt.Cache.Add(key, value) }    
-    match logWrite (key, value) with
-    | Some n -> 
-        printfn "written %A " n
-        if(n > 1024 * 4) then
-            printf "big"
-            let cache = mt.Cache
-            mt <- { mt with Cache = Map.empty }
-            //write cache to disk
-            //Map.map (fun (k, v) -> toBytes (k, v)) 
-            cache
-            |> Map.toList
-            |> List.map (fun (k, v) -> toBytes (k, v)) 
-            |> Array.concat
-            |> (fun x -> File.WriteAllBytes (@"c:\dump\naif\sstable1.sst", x))
+    match pair with
+    | (key:string, value:string) ->
+        try
+            toBytes(key, value) 
+            |> write     
+            |> Array.length
+            |> Some
+        with
+            | ex -> None
 
-            //ignore // create sstable from cache
-        
-    | None -> printf "could not write to log"
+type MemTable =  
+    { Cache : Map<string, string>; File : FileStream; Size : int64 }
+    member this.add (k,v) =
+        match logWrite (this.File, (k,v)) with
+        | Some n -> 
+            let currentSize = this.Size + (int64)n
+            { this with Cache = this.Cache.Add(k, v); Size = currentSize }
 
-let closeFile() = 
+        | None -> this
+    member x.get key =
+        x.Cache.TryFind key
+    member x.close () =
+        x.File.Close()
+        x.File.Dispose()
+    interface System.IDisposable with
+        member x.Dispose() =
+            x.File.Dispose()
+
+type MemTableResult =
+    | Mt of MemTable
+    | Sst of MemTable
+
+type MemTable with
+    member x.add2 (k, v) =
+        match logWrite (x.File, (k,v)) with
+        | Some n -> 
+            Sst x
+        | None -> Mt x
+
+let readLog (fs:Stream) =
+    let readBytes (fs:Stream, num) =
+        let bytes = Array.init num (fun i -> 0uy)
+        let read = fs.Read(bytes, 0, num)
+        bytes
+
+    let readInt (fs:Stream) = 
+        let toInt bytes = BitConverter.ToInt32(bytes, 0)
+        readBytes (fs, 4) |> toInt
+    
+    let readString (fs:Stream, num) =
+        readBytes (fs, num) |> System.Text.Encoding.UTF8.GetString
+
+    let rec read m:Map<string,string> =
+        let l = readInt fs
+        if(l = 0) then
+            m
+        else
+            let k = readString (fs, l)
+            let l = readInt fs
+            let v = readString (fs, l)
+            read (m.Add (k, v))
+    read Map.empty
+
+let openFile s =
+    if (File.Exists s) then
+        let readFs = File.OpenRead s
+        let log = readLog readFs
+        let pos = readFs.Position;
+        readFs.Close()
+        readFs.Dispose()
+
+        let fs = File.Open(s, FileMode.Append)
+        { Cache = log; File = fs; Size = pos }
+    else
+        { Cache = Map.empty; File = File.OpenWrite(s); Size = 0L }
+
+
+
+//
+//
+//let addToMemTable (fs, key, value) =
+//    //{ mt with Cache =  mt.Cache.Add(key, value) }    
+//    let lw pair = logWrite (fs, pair)
+//    match lw (key, value) with
+//    | Some n ->
+//        printfn "written %A " n
+////        if(n > 1024 * 4) then
+////            printf "big"
+////            let cache = mt.Cache
+////            mt <- { mt with Cache = Map.empty }
+////            //write cache to disk
+////            //Map.map (fun (k, v) -> toBytes (k, v)) 
+////            cache
+////            |> Map.toList
+////            |> List.map (fun (k, v) -> toBytes (k, v)) 
+////            |> Array.concat
+////            |> (fun x -> File.WriteAllBytes (@"c:\dump\naif\sstable1.sst", x))
+//
+//            //ignore // create sstable from cache
+//        
+//    | None -> printf "could not write to log"
+//    { mt with Cache =  mt.Cache.Add(key, value) }  
+
+let closeFile (fs:FileStream) = 
     fs.Close()
     fs.Dispose();
-
-let get(key) =
-    mt.Cache.TryFind key
--- src/fs/Naif/Naif/Script.fsx
+++ src/fs/Naif/Naif/Script.fsx
@@ -5,18 +5,30 @@
 #load "MemTable.fs"
 open Naif
 //open MemTable
+open System
+open System.IO
+open MemTable
 
-MemTable.openFile @"c:\dump\naif\mm1.log"
-MemTable.addToMemTable("karl", "nilsson")
-MemTable.addToMemTable("hey", "ho")
-MemTable.addToMemTable("ban", "ana")
+let put (k, v, mt:MemTable) =
+    let m = mt.add(k, v)
+    if (m.Size > 4096L) then
+        m.close()
+        printf "sstable to be created"
+        MemTable.openFile @"c:\dump\naif\mm14.log"
+    else
+        m
 
-match MemTable.get "karls" with
-| Some s -> printfn "%s" s
-| None -> printf "not found"
+let mutable mt = MemTable.openFile @"c:\dump\naif\mm14.log"
+//File.OpenRead @"c:\dump\naif\mm10.log"
+mt <- put (Guid.NewGuid().ToString(), "123412341234", mt)
+//mt <- mt.add ("ruth", "7803253514")
+//mt <- mt.add ("jasper", "0903253514")
 
-MemTable.closeFile()
+mt.close ()
 
+let m1 = Map.empty
+m1.Add(123, 123)
+m1.Add(124, 124)
 
 let b = Array.init 256 (fun i -> 0uy) //byte suffix
 
@@ -29,7 +41,25 @@ let arrs = seq {
     }
 
 Array.concat arrs
+open System
+open System.IO
+open System.ComponentModel
+open System.Text
 
+
+let mutable map = Map.empty
+map <- map.Add("b", "test")
+map <- map.Add("a", "test")
+map
+
+
+open System.IO
+
+let fs = File.Open( @"c:\dump\naif\mm1.log", FileMode.Open)
+let _m = readLog fs
+fs.Close()
+fs.Dispose();
+_m
 
 let b1 = System.Text.Encoding.UTF8.GetBytes("blah ha")
 Array.blit b1 0 b 0 b1.Length
